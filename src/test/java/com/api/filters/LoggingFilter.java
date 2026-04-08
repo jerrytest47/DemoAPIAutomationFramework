5ef014fb0588bb82947187719d302a4ae2089ca5
jerrytest47 <jerrytest47@gmail.com>
2026-04-07 23:29:21 -0600

    updated filter and listener classes

--- src/test/java/com/api/filters/LoggingFilter.java
+++ src/test/java/com/api/filters/LoggingFilter.java
@@ -1,193 +1,43 @@
 package com.api.filters;
 
-import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.Marker;
-import org.apache.logging.log4j.core.Filter;
-import org.apache.logging.log4j.core.LogEvent;
-import org.apache.logging.log4j.message.Message;
 
+import io.restassured.filter.Filter;
 import io.restassured.filter.FilterContext;
 import io.restassured.response.Response;
 import io.restassured.specification.FilterableRequestSpecification;
 import io.restassured.specification.FilterableResponseSpecification;
 
 public class LoggingFilter implements Filter {
+
     private static final Logger logger = LogManager.getLogger(LoggingFilter.class);
 
+    @Override
     public Response filter(FilterableRequestSpecification requestSpec,
-                         FilterableResponseSpecification responseSpec,
-                         FilterContext ctx) {
-// Log request
+                           FilterableResponseSpecification responseSpec,
+                           FilterContext ctx) {
+
         logRequest(requestSpec);
 
-// Get response
         Response response = ctx.next(requestSpec, responseSpec);
 
-// Log response
         logResponse(response);
 
-        return response; //test for assertion
+        return response;
     }
 
     private void logRequest(FilterableRequestSpecification requestSpec) {
-        logger.info("BASE URI:"+ requestSpec.getBaseUri());
-        logger.info("Request Header:"+ requestSpec.getHeaders());
-        logger.info("Request PayloadI:"+ requestSpec.getBody());
-
+        logger.info("Request method: {}", requestSpec.getMethod());
+        logger.info("Request URI: {}", requestSpec.getURI());
+        logger.info("Request headers: {}", requestSpec.getHeaders());
+        Object body = requestSpec.getBody();
+        logger.info("Request body: {}", body != null ? body.toString() : "null");
     }
 
     private void logResponse(Response response) {
-    	 logger.info("STATUS CODE:"+ response.getStatusCode());
-         logger.info("Response Header:"+ response.headers());
-         logger.info("Request Body:"+ response.getBody().prettyPrint());
+        logger.info("Response status code: {}", response.getStatusCode());
+        logger.info("Response headers: {}", response.getHeaders());
+        logger.info("Response body: {}", response.asString());
     }
-
-	@Override
-	public State getState() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void initialize() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void start() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void stop() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public boolean isStarted() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean isStopped() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public Result getOnMismatch() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Result getOnMatch() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Result filter(org.apache.logging.log4j.core.Logger logger, Level level, Marker marker, String msg,
-			Object... params) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Result filter(org.apache.logging.log4j.core.Logger logger, Level level, Marker marker, String message,
-			Object p0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Result filter(org.apache.logging.log4j.core.Logger logger, Level level, Marker marker, String message,
-			Object p0, Object p1) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Result filter(org.apache.logging.log4j.core.Logger logger, Level level, Marker marker, String message,
-			Object p0, Object p1, Object p2) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Result filter(org.apache.logging.log4j.core.Logger logger, Level level, Marker marker, String message,
-			Object p0, Object p1, Object p2, Object p3) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Result filter(org.apache.logging.log4j.core.Logger logger, Level level, Marker marker, String message,
-			Object p0, Object p1, Object p2, Object p3, Object p4) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Result filter(org.apache.logging.log4j.core.Logger logger, Level level, Marker marker, String message,
-			Object p0, Object p1, Object p2, Object p3, Object p4, Object p5) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Result filter(org.apache.logging.log4j.core.Logger logger, Level level, Marker marker, String message,
-			Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Result filter(org.apache.logging.log4j.core.Logger logger, Level level, Marker marker, String message,
-			Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6, Object p7) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Result filter(org.apache.logging.log4j.core.Logger logger, Level level, Marker marker, String message,
-			Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6, Object p7, Object p8) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Result filter(org.apache.logging.log4j.core.Logger logger, Level level, Marker marker, String message,
-			Object p0, Object p1, Object p2, Object p3, Object p4, Object p5, Object p6, Object p7, Object p8,
-			Object p9) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Result filter(org.apache.logging.log4j.core.Logger logger, Level level, Marker marker, Object msg,
-			Throwable t) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Result filter(org.apache.logging.log4j.core.Logger logger, Level level, Marker marker, Message msg,
-			Throwable t) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Result filter(LogEvent event) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 }
-
--- src/test/java/com/api/listeners/TestListener.java
+++ src/test/java/com/api/listeners/TestListener.java
@@ -1,48 +1,74 @@
 package com.api.listeners;
 
-
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
+public class TestListener implements ITestListener {
 
+    private static final Logger logger = LogManager.getLogger(TestListener.class);
 
-	public class TestListener implements ITestListener {
-	    private static final Logger logger = LogManager.getLogger(TestListener.class);
-	    
-	    @Override
-	    public void onStart(ITestContext context) {
-	        logger.info("Test Run Started!!");
-	      }
-	    
-	    @Override
-	    public void onTestStart(ITestResult result) {
-	        logger.info("Test Suite Started: !!", result.getMethod().getMethodName());
-	        logger.info("Description", result.getMethod().getDescription());
-	    }
+    @Override
+    public void onStart(ITestContext context) {
+        logger.info("========================================");
+        logger.info("Test run started: {}", context.getName());
+        logger.info("========================================");
+    }
 
-	    @Override
-	    public void onTestSuccess(ITestResult result) {
-	        logger.info("Test Passed:", result.getMethod().getMethodName());
-	    }
+    @Override
+    public void onTestStart(ITestResult result) {
+        logger.info("STARTED: {}.{}",
+                result.getTestClass().getName(),
+                result.getMethod().getMethodName());
 
-	    @Override
-	    public void onTestFailure(ITestResult result) {
-	        logger.info("Test Failed:", result.getMethod().getMethodName());
-	        logger.error("Exception: ", result.getThrowable());
-	    }
-	    
-	   public void onTestSkipped(ITestResult result) {
-	        logger.info("Test Skipped:", result.getMethod().getMethodName());
-	      }
-	   
-	   public void onFinish(ITestContext context) {
-		    logger.info("Test Suite Complete");
-		  }
-	}
+        String description = result.getMethod().getDescription();
+        logger.info("Description: {}", description != null ? description : "No description");
+    }
 
-	
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        logger.info("PASSED: {}.{}",
+                result.getTestClass().getName(),
+                result.getMethod().getMethodName());
 
+        logger.info("Duration: {} ms", getExecutionTime(result));
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        logger.error("FAILED: {}.{}",
+                result.getTestClass().getName(),
+                result.getMethod().getMethodName());
+
+        logger.error("Duration: {} ms", getExecutionTime(result));
+
+        if (result.getThrowable() != null) {
+            logger.error("Failure reason:", result.getThrowable());
+        }
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        logger.warn("SKIPPED: {}.{}",
+                result.getTestClass().getName(),
+                result.getMethod().getMethodName());
+
+        logger.warn("Duration: {} ms", getExecutionTime(result));
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        logger.info("========================================");
+        logger.info("Test run finished: {}", context.getName());
+        logger.info("Passed: {}", context.getPassedTests().size());
+        logger.info("Failed: {}", context.getFailedTests().size());
+        logger.info("Skipped: {}", context.getSkippedTests().size());
+        logger.info("========================================");
+    }
+
+    private long getExecutionTime(ITestResult result) {
+        return result.getEndMillis() - result.getStartMillis();
+    }
+}
